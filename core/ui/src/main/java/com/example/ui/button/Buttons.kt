0e7c2e1764a8d08de8bc0968f279b2344e094524
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2025-05-03 20:48:53 +0500

    add TextButton

--- core/ui/src/main/java/com/example/ui/button/Buttons.kt
+++ core/ui/src/main/java/com/example/ui/button/Buttons.kt
@@ -1,12 +1,20 @@
 package com.example.ui.button
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.designsystem.theme.AppTheme
@@ -30,7 +38,7 @@ fun MainButton(
         ),
         contentPadding = PaddingValues(vertical = 10.dp),
         onClick = onClick,
-    ){
+    ) {
         Text(
             text = text,
             style = AppTheme.typography.button,
@@ -39,12 +47,40 @@ fun MainButton(
     }
 }
 
+@Composable
+fun TextButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Text(
+        modifier = modifier
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = { onClick() }
+            ),
+        text = text,
+        style = AppTheme.typography.buttonSmall,
+        color = AppTheme.colors.green,
+    )
+}
+
 @Preview
 @Composable
 private fun ButtonsPreview() {
-    MainButton(
-        variant = MainButtonVariant.Secondary,
-        onClick = {},
-        text = "Начать курс"
-    )
+    Column {
+        MainButton(
+            variant = MainButtonVariant.Secondary,
+            onClick = {},
+            text = "Начать курс"
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        TextButton(
+            text = "Забыл пароль",
+            onClick = {}
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+
 }
